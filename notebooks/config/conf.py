a605dca9e382d53bff7ec3a5c7d7395913c64455
dfguerrerom <dfgm2006@gmail.com>
2022-07-08 07:59:05 +0000

    tune: change parameters to fit different image sizes

--- notebooks/config/conf.py
+++ notebooks/config/conf.py
@@ -10,13 +10,13 @@ class Configuration:
         self.training_dir = self.base_dir/"training_afk/"
         
         self.train_image_dir = self.training_dir/"0_raw/images"
-        self.annotation_dir = self.training_dir/"0_raw/annotation"
-        self.boundary_dir = self.training_dir/"0_raw/boundary"
+        self.annotation_dir = self.training_dir/"1_processed/annotation"
+        self.boundary_dir = self.training_dir/"1_processed/boundary"
         
         self.image_dir = self.training_dir/"1_processed/image"
         
-        self.training_area = self.training_dir/"0_raw/1_input_data/220614_first_batch/220614_areas.shp"
-        self.training_polygon = self.training_dir/"0_raw/1_input_data/220614_first_batch/220614_polygons_4326.shp"
+        self.training_area = self.training_dir/"0_raw/1_input_data/4th_batch/areas.shp"
+        self.training_polygon = self.training_dir/"0_raw/1_input_data/4th_batch/trees.shp"
 
         self.model_dir = self.training_dir/"saved_models/UNet"
         
@@ -63,7 +63,10 @@ class Configuration:
         # patches are returned in one call.
         
         self.patch_generation_stratergy = "random"  # 'random' or 'sequential'
-        self.patch_size = (256, 256, 10)  # Height * Width * (Input + Output) channels
+        
+        # Height * Width * (Input + Output) channels
+        self.patch_size = (64, 64, len(self.bands)+2)  
+        
         # # When stratergy == sequential, then you need the step_size as well
         # step_size = (128,128)
 
@@ -79,19 +82,19 @@ class Configuration:
         self.normalize = 0.4
 
         # Shape of the input data, height*width*channel; Here channels are NVDI and Pan
-        self.input_shape = (256, 256, 8)
-        self.input_image_channel = [0, 1, 2, 3]
-        self.input_label_channel = [4]
-        self.input_weight_channel = [5]
+        self.input_shape = (64, 64, len(self.bands))
+        self.input_image_channel = self.bands
+        self.input_label_channel = [max(self.bands)+1]
+        self.input_weight_channel = [max(self.bands)+2]
 
         # CNN model related variables used in the notebook
-        self.BATCH_SIZE = 8
+        self.BATCH_SIZE = 10
         self.NB_EPOCHS = 200
 
         # number of validation images to use
         self.VALID_IMG_COUNT = 200
         # maximum number of steps_per_epoch in training
-        self.MAX_TRAIN_STEPS = 1000
+        self.MAX_TRAIN_STEPS = 96
 
         
         # Input related variables
@@ -105,11 +108,11 @@ class Configuration:
         self.output_dtype = "uint8"
 
         # Variables related to batches and model
-        self.BATCH_SIZE = 200  # Depends upon GPU memory and WIDTH and HEIGHT 
+        self.BATCH_SIZE_PREDICT = 200  # Depends upon GPU memory and WIDTH and HEIGHT 
         # (Note: Batch_size for prediction can be different then for training.
-        self.WIDTH = 256  # Should be same as the WIDTH used for training the model
-        self.HEIGHT = 256  # Should be same as the HEIGHT used for training the model
-        self.STRIDE = 224  # 224 or 196   # STRIDE = WIDTH means no overlap, 
+        self.WIDTH = 64  # Should be same as the WIDTH used for training the model
+        self.HEIGHT = 64  # Should be same as the HEIGHT used for training the model
+        self.STRIDE = 64  # 224 or 196   # STRIDE = WIDTH means no overlap, 
         # STRIDE = WIDTH/2 means 50 % overlap in prediction
         
         paths = [item for item in self.__dict__.values() if isinstance(item, Path)]
